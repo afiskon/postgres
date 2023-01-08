cdf59e8a4a9aee58a67b35cca4c1a2bbe02f770d
Alexander Korotkov <akorotkov@postgresql.org>
2023-01-09 01:02:12 +0300

    Allow locking updated tuples in tuple_update() and tuple_delete()
    Currently, in read committed transaction isolation mode (default), we have the
    following sequence of actions when tuple_update()/tuple_delete() finds
    the tuple updated by concurrent transaction.
    
     1. Attempt to update/delete tuple with tuple_update()/tuple_delete(), which
        returns TM_Updated.
     2. Lock tuple with tuple_lock().
     3. Re-evaluate plan qual (recheck if we still need to update/delete and
        calculate the new tuple for update).
     4. Second attempt to update/delete tuple with tuple_update()/tuple_delete().
        This attempt should be successful, since the tuple was previously locked.
    
    This patch eliminates step 2 by taking the lock during first
    tuple_update()/tuple_delete() call.  Heap table access methods could save
    efforts by traversing chain of updated tuples once instead of twice.  Future
    undo-based table access methods, which will start from the lastest row version.
    can immediately place a lock there.
    
    The code in nodeModifyTable.c is simplified by removing the nested switch/case.
    Also, we now table_tuple_fetch_row_version() in ExecUpdate(), because old
    version of row should be always the same that we've previously locked.
    
    Discussion: https://postgr.es/m/CAPpHfdua-YFw3XTprfutzGp28xXLigFtzNbuFY8yPhqeq6X5kg%40mail.gmail.com
    Reviewed-by: Aleksander Alekseev, Pavel Borisov, Vignesh C, Mason Sharp

--- src/include/access/tableam.h
+++ src/include/access/tableam.h
@@ -514,7 +514,9 @@ typedef struct TableAmRoutine
 								 Snapshot crosscheck,
 								 bool wait,
 								 TM_FailureData *tmfd,
-								 bool changingPart);
+								 bool changingPart,
+								 bool lockUpdated,
+								 TupleTableSlot *lockedSlot);
 
 	/* see table_tuple_update() for reference about parameters */
 	TM_Result	(*tuple_update) (Relation rel,
@@ -526,7 +528,9 @@ typedef struct TableAmRoutine
 								 bool wait,
 								 TM_FailureData *tmfd,
 								 LockTupleMode *lockmode,
-								 bool *update_indexes);
+								 bool *update_indexes,
+								 bool lockUpdated,
+								 TupleTableSlot *lockedSlot);
 
 	/* see table_tuple_lock() for reference about parameters */
 	TM_Result	(*tuple_lock) (Relation rel,
@@ -1449,6 +1453,9 @@ table_multi_insert(Relation rel, TupleTableSlot **slots, int nslots,
  *	tmfd - filled in failure cases (see below)
  *	changingPart - true iff the tuple is being moved to another partition
  *		table due to an update of the partition key. Otherwise, false.
+ *	lockUpdated - true if should lock the last row version if the concurrent
+ *		update is detected
+ *	lockedSlot - slot to save the locked tuple when lockUpdated is true
  *
  * Normal, successful return value is TM_Ok, which means we did actually
  * delete it.  Failure return codes are TM_SelfModified, TM_Updated, and
@@ -1461,11 +1468,13 @@ table_multi_insert(Relation rel, TupleTableSlot **slots, int nslots,
 static inline TM_Result
 table_tuple_delete(Relation rel, ItemPointer tid, CommandId cid,
 				   Snapshot snapshot, Snapshot crosscheck, bool wait,
-				   TM_FailureData *tmfd, bool changingPart)
+				   TM_FailureData *tmfd, bool changingPart,
+				   bool lockUpdated, TupleTableSlot *lockedSlot)
 {
 	return rel->rd_tableam->tuple_delete(rel, tid, cid,
 										 snapshot, crosscheck,
-										 wait, tmfd, changingPart);
+										 wait, tmfd, changingPart,
+										 lockUpdated, lockedSlot);
 }
 
 /*
@@ -1487,6 +1496,9 @@ table_tuple_delete(Relation rel, ItemPointer tid, CommandId cid,
  *	lockmode - filled with lock mode acquired on tuple
  *  update_indexes - in success cases this is set to true if new index entries
  *		are required for this tuple
+ *	lockUpdated - true if should lock the last row version if the concurrent
+ *		update is detected
+ *	lockedSlot - slot to save the locked tuple when lockUpdated is true
  *
  * Normal, successful return value is TM_Ok, which means we did actually
  * update it.  Failure return codes are TM_SelfModified, TM_Updated, and
@@ -1506,12 +1518,14 @@ static inline TM_Result
 table_tuple_update(Relation rel, ItemPointer otid, TupleTableSlot *slot,
 				   CommandId cid, Snapshot snapshot, Snapshot crosscheck,
 				   bool wait, TM_FailureData *tmfd, LockTupleMode *lockmode,
-				   bool *update_indexes)
+				   bool *update_indexes, bool lockUpdated,
+				   TupleTableSlot *lockedSlot)
 {
 	return rel->rd_tableam->tuple_update(rel, otid, slot,
 										 cid, snapshot, crosscheck,
 										 wait, tmfd,
-										 lockmode, update_indexes);
+										 lockmode, update_indexes,
+										 lockUpdated, lockedSlot);
 }
 
 /*
