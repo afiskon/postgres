23aa542f0623a779238100ceb215f773380a2d2d
Aleksander Alekseev <aleksander@timescale.com>
2024-09-11 13:17:33 +0300

    Always use long SLRU segment file names
    PG17 introduced long SLRU segment file names (commit 4ed8f0913bfd). We used
    short or long file names depending on SlruCtl->long_segment_names. This commit
    refactors SLRU to always use long file names in order to simplify the code.
    
    Aleksander Alekseev, reviewed by Michael Paquier
    Discussion: https://postgr.es/m/CAJ7c6TOy7fUW9MuNeOWor3cSFnQg9tgz=mjXHDb94GORtM_Eyg@mail.gmail.com

---
+++ src/bin/pg_upgrade/t/005_slru.pl
@@ -0,0 +1,59 @@
+# Copyright (c) 2024, PostgreSQL Global Development Group
+
+use strict;
+use warnings FATAL => 'all';
+
+use PostgreSQL::Test::Cluster;
+use PostgreSQL::Test::Utils;
+use Test::More;
+
+# This test ensures that pg_upgrade renames SLRU segments.
+# After the upgrade all segments should have long file names.
+
+my @slru_dirs = (
+	"pg_xact",
+#	"pg_commit_ts", # is empty
+	"pg_multixact/offsets",
+	"pg_multixact/members",
+#	"pg_subtrans", # only has 000000000000000
+#	"pg_serial", # is empty
+);
+
+my $short_segment_name = "1234";
+my $long_segment_name = "000000000001234";
+
+my $oldnode = PostgreSQL::Test::Cluster->new('old_node');
+$oldnode->init();
+my $oldbindir = $oldnode->config_data('--bindir');
+
+my $newnode = PostgreSQL::Test::Cluster->new('new_node');
+$newnode->init();
+my $newbindir = $newnode->config_data('--bindir');
+
+# Fill data_dir of the old node with SLRU segments that use short file names.
+# pg_upgeade renames the files without looking at the content, so the content
+# is not important.
+foreach my $dir (@slru_dirs)
+{
+	my $fname = $oldnode->data_dir."/".$dir."/".$short_segment_name;
+	open my $fh, ">", $fname or die $!;
+	close $fh;
+}
+
+command_ok(
+	[
+		$newbindir.'/pg_upgrade',
+		'--old-datadir', $oldnode->data_dir,
+		'--new-datadir', $newnode->data_dir,
+		'--old-bindir', $oldbindir,
+		'--new-bindir', $newbindir,
+	],
+	'run of pg_upgrade');
+
+# Check that pg_upgrade renamed the SLRU segments we created
+foreach my $dir (@slru_dirs)
+{
+	ok(-e $newnode->data_dir."/".$dir."/".$long_segment_name);
+}
+
+done_testing();
