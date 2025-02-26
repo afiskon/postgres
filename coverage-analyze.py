021255212ed07bbacb0300375c713eba94ed65c6
Aleksander Alekseev <aleksander@timescale.com>
2025-02-26 14:22:23 +0300

    fix

--- coverage-analyze.py
+++ coverage-analyze.py
@@ -1,4 +1,4 @@
-#!/usr/bin/enb python3
+#!/usr/bin/env python3
 
 # coverage-analyze.py
 # (c) Aleksander Alekseev 2025
@@ -11,7 +11,7 @@ functions = set()
 
 with open("src/include/catalog/pg_proc.dat") as proc_f:
 	proc_data = proc_f.read()
-	re_str = """proname\s+=>\s+['"](.*?)['"]"""
+	re_str = """proname[ ]+=>[ ]+['"](.*?)['"]"""
 	for m in re.finditer(re_str, proc_data):
 		func_name = m.group(1)
 		functions.add(func_name)
