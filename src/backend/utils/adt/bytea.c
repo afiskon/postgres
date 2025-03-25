ae7f0165e0858a2dd617726b85469e52c383c4dd
Aleksander Alekseev <aleksander@timescale.com>
2025-03-25 13:01:31 +0300

    fix

---
+++ src/backend/utils/adt/bytea.c
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------
+ *
+ * bytea.c
+ *	  Functions for the bytea type.
+ *
+ * Portions Copyright (c) 2025, PostgreSQL Global Development Group
+ *
+ *
+ * IDENTIFICATION
+ *	  src/backend/utils/adt/bytea.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
